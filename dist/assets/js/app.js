cb4ae0ced869b869bc4f4b54ac327f0124db3b09
rvitalia <rvitalia@mail.ru>
2024-02-24 21:16:02 +0300

    первый блок

---
+++ dist/assets/js/app.js
@@ -0,0 +1,1 @@
+$("#select_money_receive").ddslick({onSelected:function(e){}}),$("#select_money_contry").ddslick({onSelected:function(e){}}),$("#select_money_valuta").ddslick({onSelected:function(e){}});
