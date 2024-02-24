628e300b8582b2d17c353bc66472c2f682d62d7f
rvitalia <rvitalia@mail.ru>
2024-02-24 21:03:46 +0300

    переводы 1 блок

---
+++ src/assets/js/app.js
@@ -0,0 +1,15 @@
+$('#select_money_receive').ddslick({
+    onSelected: function(selectedData){
+        //callback function: do something with selectedData;
+    }   
+});
+$('#select_money_contry').ddslick({
+    onSelected: function(selectedData){
+        //callback function: do something with selectedData;
+    }   
+});
+$('#select_money_valuta').ddslick({
+    onSelected: function(selectedData){
+        //callback function: do something with selectedData;
+    }   
+});
