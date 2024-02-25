9d856cbf557cf942ac78724873ffc605ea3f8e94
rvitalia <rvitalia@mail.ru>
2024-02-25 11:13:10 +0300

    desktop

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,18 +1,31 @@
 $('#select_money_receive').ddslick({
-    onSelected: function(selectedData){
+    onSelected: function (selectedData) {
         //callback function: do something with selectedData;
-    }   
+    }
 });
 $('#select_money_contry').ddslick({
-    onSelected: function(selectedData){
+    onSelected: function (selectedData) {
         //callback function: do something with selectedData;
-    }   
+    }
 });
 $('#select_money_valuta').ddslick({
-    onSelected: function(selectedData){
+    onSelected: function (selectedData) {
         //callback function: do something with selectedData;
-    }   
+    }
 });
-$( function() {
-    $( "#accordion" ).accordion();
-  } );
+$(function () {
+    $("#accordion").accordion();
+});
+$('#partners__slider').slick({
+    // centerMode: true,
+    arrows: false,
+    centerPadding: '60px',
+    dots: true,
+})
+
+$('.review__inner').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
