abd4e1b2bb487d81b38a051885c2745c93b670fa
rvitalia <rvitalia@mail.ru>
2024-02-25 14:14:13 +0300

    mark and mobile version

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -17,10 +17,30 @@ $(function () {
     $("#accordion").accordion();
 });
 $('#partners__slider').slick({
-    // centerMode: true,
+    centerMode: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
     arrows: false,
     centerPadding: '60px',
     dots: true,
+    responsive: [
+        {
+          breakpoint: 992,
+          settings: {
+            slidesToShow: 2,
+            centerPadding: '0',
+            centerMode: false,
+          }
+        },
+        {
+          breakpoint: 576,
+          settings: {
+            slidesToShow: 1,
+            centerMode: true,
+            centerPadding: '40px',
+          }
+        }
+      ]
 })
 
 $('.review__inner').slick({
@@ -28,4 +48,37 @@ $('.review__inner').slick({
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 2000,
+    arrows: false,
+    dots: true,
+    responsive: [
+        {
+          breakpoint: 992,
+          settings: {
+            slidesToShow: 3
+          }
+        },
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 2
+          }
+        },
+        {
+          breakpoint: 576,
+          settings: {
+            slidesToShow: 1,
+            centerMode: true,
+          }
+        }
+      ]
   });
+
+  $('.help__inner__item-mobile').slick({
+    slidesToShow:1,
+    slidesToScroll: 1,
+    centerMode: true,
+    // autoplay: true,
+    // autoplaySpeed: 2000,
+    arrows: false,
+    dots: true,
+  })
