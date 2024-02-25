cd842657ca5c1604ca59db14feabff78e183281e
rvitalia <rvitalia@mail.ru>
2024-02-25 07:49:07 +0300

    4 секция

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -13,3 +13,20 @@ $('#select_money_valuta').ddslick({
         //callback function: do something with selectedData;
     }   
 });
+
+// let rowSelected = document.querySelectorAll('[data-selected]');
+// let imageSelected = document.querySelectorAll('[data-selectimage]');
+
+// let screenSize= window.innerWidth;
+// if(screenSize > 769){
+//     rowSelected.forEach(element => {
+//         element.addEventListener('mousemove',()=>{
+//             element.style.color = 'white';
+//             element.style.background = 'black';
+//         });
+//         element.addEventListener('mouseleave',()=>{
+//             element.style.color = 'black';
+//             element.style.background = '#e7e7f0';
+//         });
+//     });
+// }
